45cd80f2feeeccdc844eecfda2089fb9031acafd
Ksusha <ks.romanchik@gmail.com>
2017-04-19 09:28:26 +0300

    add install worker

---
+++ serviceWorkerInit.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+    console.log('install', event);
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('activate', event);
+});
